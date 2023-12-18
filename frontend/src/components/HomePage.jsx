11e31ff36d713888300ca9e04f8c836dcc4bf3bf
Veloxal <dulransamarasinghe4@gmail.com>
2023-12-18 17:36:48 +0530

    minorChanges

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = (props) => {
   const { user, isLogged } = props;
+  const [time, setTime] = useState("day");
+
+  const date = new Date();
+  const hour = date.getHours();
+
+  // if (hour < 12) {
+  //   setTime("Morning");
+  // } else if (hour < 17) {
+  //   setTime("Afternoon");
+  // } else if (hour < 20) {
+  //   setTime("Evening");
+  // } else {
+  //   setTime("Night");
+  // }
+
   return (
     <div>
       {isLogged ? (
         <div>
-          <h1>Welcome {user} ! Select your choice</h1>
+          <h1>
+            Good {time} Welcome {user.username}! Please select your choice{" "}
+          </h1>
           <button>
             <Link to="/stat">StatPage</Link>
           </button>
@@ -16,10 +34,11 @@ const HomePage = (props) => {
         </div>
       ) : (
         <div>
-          <h1>Welcome to (CS63 Project Name)!</h1>
+          <h1>Good {time}!, Welcome to (CS63 Platform Name)!</h1>
           <h2 style={{ margin: "5%" }}>
             Our platform hopes to help you improve your maths skills , with the
-            help of personalized learning ✨
+            help of personalized learning ✨ . So join us on this adventure to
+            ace your A-Levels like a Champ 🏆
           </h2>
           <button>
             <Link path="/adduser">Click Here to Register</Link>
