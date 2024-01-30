634e18d8524d1dbf26b75419f2ad2b44c26f8caf
Veloxal <dulransamarasinghe4@gmail.com>
2024-01-30 13:34:16 +0530

    topicWiseMinorFix

--- backend/routes/examStat.js
+++ backend/routes/examStat.js
@@ -6,13 +6,17 @@ router
   .route("/")
   .get(async (req, res) => {
     const { topic } = req?.body;
-    if (!topic) return res.status(400).json({ Alert: "No topic selected!" });
-
-    const topicExists = await statModel.find({ topic });
     if (!topic) {
-      return res.status(400).json({ Alert: "Topic doesn't exist!" });
+      return res
+        .status(200)
+        .json(Math.random(statModel.find().sort("createdAt")));
     } else {
-      return res.status(200).json(Math.random(topicExists));
+      const topicExists = await statModel.find({ $match: { topic } });
+      if (!topic) {
+        return res.status(400).json({ Alert: "Topic doesn't exist!" });
+      } else {
+        return res.status(200).json(Math.random(topicExists));
+      }
     }
   })
   .post(/**Student answering and sending back question logic! (comparison) */);
